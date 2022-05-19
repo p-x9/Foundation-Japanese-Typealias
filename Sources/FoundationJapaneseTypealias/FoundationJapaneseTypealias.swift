1117ca53f7eae6c1d282a439b48e9087be35b608
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-05-20 00:11:04 +0900

    add typealias

--- Sources/FoundationJapaneseTypealias/FoundationJapaneseTypealias.swift
+++ Sources/FoundationJapaneseTypealias/FoundationJapaneseTypealias.swift
@@ -1,6 +1,20 @@
-public struct Foundation_Japanese_Typealias {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
-}
+public typealias 真理値 = Bool
+public typealias 整数 = Int
+public typealias 単精度浮動小数 = Float
+public typealias 倍精度浮動小数 = Double
+
+public typealias 文字列 = String
+public typealias 文字 = Character
+
+public typealias 配列 = Array
+public typealias セット = Set
+public typealias 辞書 = Dictionary
+
+public typealias 日付 = Date
+public typealias カレンダー = Calendar
+
+public typealias データ = Data
+
+
--- Tests/FoundationJapaneseTypealiasTests/Foundation_Japanese_TypealiasTests.swift
+++ Tests/FoundationJapaneseTypealiasTests/Foundation_Japanese_TypealiasTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import FoundationJapaneseTypealias
 
 final class FoundationJapaneseTypealiasTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Foundation_Japanese_Typealias().text, "Hello, World!")
-    }
 }
